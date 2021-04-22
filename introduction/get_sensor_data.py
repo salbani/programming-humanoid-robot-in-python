3bc8d0d0b4c36e3a7633d8661d89f7b861688855
Simon Albani <simon@albani-mail.de>
2021-04-22 16:06:09 +0200

    removed JOINT_SENSOR_NAMES

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -14,7 +14,6 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '
 
 from spark_agent import SparkAgent
 from spark_agent import Perception
-from spark_agent import JOINT_SENSOR_NAMES
 
 
 class MyAgent(SparkAgent):
@@ -23,8 +22,8 @@ class MyAgent(SparkAgent):
         temperature = 0
         # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
-        temperature = perception.joint_temperature[JOINT_SENSOR_NAMES["hj1"]]
-        angle = perception.joint[JOINT_SENSOR_NAMES["hj1"]]
+        temperature = perception.joint_temperature['HeadYaw']
+        angle = perception.joint['HeadYaw']
         
         print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
         return super(MyAgent, self).think(perception)
