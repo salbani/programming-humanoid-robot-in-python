79a366a9e73fe54df4bcba1989ae371c87212134
Simon Albani <simon@albani-mail.de>
2021-05-18 14:38:50 +0200

    added backup values

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 15
-        self.Ki = 0.4
-        self.Kd = 0.1
+        self.Kp = 15 # 20
+        self.Ki = 0.4 # -0.1
+        self.Kd = 0.1 # 0.1
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
